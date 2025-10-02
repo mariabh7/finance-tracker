0d805a27ee75663e269fc5d3e4caf5c23e7cd39f
Maria <bouhrem.maria@gmail.com>
2025-10-02 11:32:18 +0100

    feat: add slider content

--- src/components/signUp/slider.jsx
+++ src/components/signUp/slider.jsx
@@ -1,6 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { useRef } from "react";
-const data = [1, 2, 3, 4, 5];
+const data = [
+  {
+    type: "comment",
+    content: {
+      comment:
+        "this finance tracker is really well done! The layout is clean and intuitive, which makes it easy to follow where the money goes. I like how things are organized here, and the automatic updates give a smooth experience. It’s simple enough for a beginner to use",
+      UserName: "maria bouhrem",
+    },
+  },
+  {
+    type: "statistics#1",
+    content: {
+      num: "2000",
+      title: " budget overview  ",
+      desc: "set a budget and get the details for each one ",
+      data: ["shopping", "education", "healthCare"],
+    },
+  },
+  {
+    type: "statistics#2",
+    content: {
+      num: "1000",
+      title: " income and expenses ",
+      desc: "track your incomes and expenses efficiently with finance tracker",
+      data: [5000, 3000, 2000, 1000],
+    },
+  },
+  {
+    type: "statistics#3",
+    content: {
+      num: "5000",
+      title: "annual and mothly analytics ",
+      desc: "we provide monthly and yearly analytics so that you can track your finances efficiently",
+      data: [5000, 3000, 2000, 1000],
+    },
+  },
+];
+function DisplayComment({ Content = {} }) {
+  return (
+    <div className="flex flex-col gap-5">
+      <div>
+        <q className="text-2xl font-bold first-letter:uppercase">
+          {Content.comment}
+        </q>
+      </div>
+      <div className="flex items-center justify-start gap-5 ">
+        <div className="w-10 h-10 rounded-full  bg-gray-400"></div>
+        <div>{Content?.UserName}</div>
+      </div>
+    </div>
+  );
+}
+function DisplayStatistic({ Content = {} }) {
+  return (
+    <div className="flex flex-col gap-5 justify-start">
+      <div className="self-start">
+        <h3 className="text-xl">{Content?.title}</h3>
+      </div>
+      <div className="flex flex-col justify-start gap-2 ">
+        <span className="self-start  text-5xl">${Content?.num}</span>
+        <p className="self-start text-gray-300">{Content?.desc}</p>
+      </div>
+    </div>
+  );
+}
 const Slider = () => {
   const [current, setCurrent] = useState(0);
   useEffect(() => {
@@ -13,16 +77,22 @@ const Slider = () => {
   return (
     <div className="absolute top-0  py-5 left-[20%] overflow-y-scroll h-screen ">
       <ul className="flex gap-2 h-fit flex-col">
-        {data.map((item) => {
+        {data.map((item, index) => {
           return (
             <li
               style={{
                 transform: `translateY(-${current * 100}%)`,
               }}
-              key={item}
-              className="temp transition-transform duration-700 ease-in-out"
+              key={item.type}
+              className={`temp transition-all duration-800 ease-in-out ${
+                item == data[current] ? "opacity-100" : "opacity-35"
+              }`}
             >
-              {item}
+              {item.type == "comment" ? (
+                <DisplayComment Content={item.content} />
+              ) : (
+                <DisplayStatistic Content={item.content} />
+              )}
             </li>
           );
         })}
